050d7e6d54696610349bc204d2e6ca460dcda481
Francisco Miguel Such Llinás <68127766+CurroSuch@users.noreply.github.com>
2021-11-26 20:24:06 +0100

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,17 +24,9 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                  // Creates `style` nodes from JS strings
-                  "style-loader",
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
-                ],
-              },
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
         ]
     },
     plugins: [
